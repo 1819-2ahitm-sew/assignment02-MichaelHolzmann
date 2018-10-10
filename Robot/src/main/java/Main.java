d01ccc79019d260ca3a8d4cca81ae1d9a6beeb3d
MichaelHolzmann <m.holzmann@htl-leonding.ac.at>
2018-10-10 10:48:41 +0200

    final version

--- Robot/src/main/java/Main.java
+++ Robot/src/main/java/Main.java
@@ -1,9 +1,22 @@
+import at.htl.robot.model.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // Do something with the robot
+        Robot robot = new Robot();
+        /*robot.setX(0);
+        robot.setY(0);
+        robot.setDirection(Direction.SOUTH);*/
 
+        robot.stepForward();
+        robot.stepForward();
+        System.out.println("("+robot.getX()+", "+robot.getY()+")");
+
+        robot.rotateLeft();
+        robot.rotateLeft();
+        robot.stepForward();
+        System.out.println("("+robot.getX()+", "+robot.getY()+")");
     }
 
 }
